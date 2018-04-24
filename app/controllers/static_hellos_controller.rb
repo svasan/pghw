944168c6cc33d3139face0d2953eac6daf3d5bf4
Sundar Vasan <vr.sundar@gmail.com>
2018-04-24 11:28:03 -0700

    Add logging

--- app/controllers/static_hellos_controller.rb
+++ app/controllers/static_hellos_controller.rb
@@ -1,6 +1,7 @@
 class StaticHellosController < ApplicationController
 
   before_action :check_accept, only: [:index, :create]
+  before_action :log, only: [:index, :create]
 
   def index
     @needs_json ? render_json : render_html
@@ -22,5 +23,9 @@ class StaticHellosController < ApplicationController
 
   def render_json
     render json: {message: "Good Morning"}
+  end
+
+  def log
+    logger.debug "#{Time.now} #{request.original_url}"
   end
 end
